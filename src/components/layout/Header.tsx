e4bcb8955928876ca23dc8927a11a7c70e8545fd
dilmurodivcc <zufarovamohira32gmail.com>
2025-03-26 11:50:48 +0500

    bercel error solved

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import avatar from "../../assets/images/avatar.svg";
 import { useNavigate } from "react-router-dom";
--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
